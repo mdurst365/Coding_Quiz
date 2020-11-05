6dc2e8aa5b116949a6ba68bb37896bddcf077af3
Molly Durst <mollymadegraphics@yahoo.com>
2020-11-04 19:18:40 -0500

    created js file for questions

---
+++ Assets/questions.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+
+var questions = {
+	title: "Arrays in JavaScript can be used to store",
+	choices: ["Numbers and Strings","Other Arrays","Booleans","All of the Above"]
+	answer: "All of the Above"
+	
+},
+	
+{
+	title: "Commonly used data types DO NOT include",
+	choices: ["Strings", "Booleans", "Alerts", "Numbers"]
+	answer: "Alerts"
+	
+},	
+	
+	{
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+      answer: "Parentheses"
+    },
+	
+		{
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+      answer: "Parentheses"
+    },
+	
+	{
+      title: "A very useful tool used during development and debugging for printing content to the debugger is",
+      choices: ["JavaScript", "Terminal / Bash", "For Loops", "Console.log"],
+      answer: "Console.log"
+    },
